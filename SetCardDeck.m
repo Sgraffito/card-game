f30fb6211b65f51c82a57143cab4eb64818dc8dd
Nicole <Nicole2@Home-Server.local>
2014-06-05 10:57:21 -0400

    SetCardDeck.m

--- SetCardDeck.m
+++ SetCardDeck.m
@@ -2,12 +2,37 @@
 //  SetCardDeck.m
 //  CardGame
 //
-//  Created by Nicole on 6/5/14.
+//  Created by Nicole on 6/2/14.
 //  Copyright (c) 2014 nicole. All rights reserved.
 //
 
 #import "SetCardDeck.h"
+#import "SetCard.h"
 
-@implementation SetCardDeck : Deck
+@implementation SetCardDeck
+
+- (instancetype)init {
+    
+    self = [super init];
+    
+    if (self) {
+        for (NSString *symbol in [SetCard validSymbols]) {
+            for (NSUInteger number = 1; number <= [SetCard maxNumber]; number++) {
+                for (UIColor *color in [SetCard colorValues]) {
+                    for (NSString *shading in [SetCard shadingStrings]) {
+                        SetCard *card = [[SetCard alloc] init];
+                        card.symbol = symbol;
+                        card.number = number;
+                        card.color = color;
+                        card.shading = shading;
+                        [self addCard:card];
+                    }
+                }
+            }
+        }
+    }
+    
+    return self;
+}
 
 @end
