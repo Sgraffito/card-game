956ecf82b75f319a6a1d5e73cfad6dc917ccbd02
Nicole <Nicole2@Home-Server.local>
2014-06-05 11:02:57 -0400

    cardGameViewController.h

--- CardGame/cardGameViewController.h
+++ CardGame/cardGameViewController.h
@@ -5,9 +5,19 @@
 //  Created by Nicole on 5/13/14.
 //  Copyright (c) 2014 nicole. All rights reserved.
 //
+//  Abstract class. Must implement methods as described below.
 
 #import <UIKit/UIKit.h>
+#import "Deck.h"
 
 @interface cardGameViewController : UIViewController
+
+// Protected
+// For subclasses
+- (Deck *)createDeck; // Abstract
+
+- (UIImage *)backgroundImageForCard:(Card *)card; // Abstract
+- (NSAttributedString *)titleForCard:(Card *)card; // Abstract
+- (int)cardMatchingCount; //Abstract
 
 @end
