1705c7f01a65c62bfee44c11c1fd7bfaa64b28c8
Nicole <Nicole2@Home-Server.local>
2014-06-05 10:44:44 -0400

    Updated PlayingCards.h

--- CardGame/Model/PlayingCard.h
+++ CardGame/Model/PlayingCard.h
@@ -11,8 +11,7 @@
 @interface PlayingCard : Card
 
 /* Suit contains a single character corrrespoinding
- * to the suit (i.e. a club, spade, heart or diamond 
- */
+ * to the suit (i.e. a club, spade, heart or diamond */
 @property (strong, nonatomic) NSString *suit;
 
 // Rank is an integer from 0 (rank not set) to 13 (King)
